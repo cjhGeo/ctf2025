3f15cc496924c4f7fcc452e5f1289e4412eba232
Chong Jian Hang Geoffrey <107737553+cjhGeo@users.noreply.github.com>
2025-06-20 22:01:46 +0800

    Update pyjcgraderlib.py

--- pyjcgraderlib.py
+++ pyjcgraderlib.py
@@ -3,54 +3,18 @@ from hashlib import blake2s as _blake2s
 from time import time as _time
 
 class DSGraderClient:
-  def __init__(self, endpoint, userToken):
+  def __init__(self, endpoint):
     self._endpoint = endpoint
-    self._userToken = userToken
     print("DSGraderClient:", _requests.get(self._endpoint+'ping').text)
-    print("[SERVER]", self.reqMessage())
-
-  def proofOfWork(self, challstr, complexity = 20, timeoutsec = 180):
-    TIMEOUT = _time() + timeoutsec
-    nonce = 0
-    while _time() < TIMEOUT:
-      hashout = _blake2s(bytes(challstr + str(nonce), 'utf-8')).digest()
-      if sum(hashout[:complexity//8]) + (hashout[complexity//8] >> (8 - complexity%8)) == 0:
-        return nonce, hashout
-      nonce += 1
-    return "TIMEOUT: proofOfWork()"
-
-  def reqChallenge(self):
-    packet = { "userToken": self._userToken }
-    try:
-      return _requests.get(self._endpoint+'proofofworkrequest', params=packet).json()
-    except:
-      print('Error requesting ProofOfWork challenge')
-
-  def reqMessage(self):
-    try:
-      return _requests.get(self._endpoint+'motd').text
-    except:
-      print('Error requesting ProofOfWork MOTD')
-
-  def findProofOfWork(self):
-    challenge = self.reqChallenge()
-    print('ProofOfWork Challenge => ', (challenge['chalstr'], challenge['complexity']))
-    answer, hashout = self.proofOfWork(challenge['chalstr'], challenge['complexity'])
-    print('ProofOfWork Answer Found! => ', answer)
-    return answer
 
   def submitFile(self, file):
-    packet = {
-      "userToken": self._userToken,
-      "answer": self.findProofOfWork()
-    }
     try:
-      return _requests.post(self._endpoint+'proofofworkupload', files={'uploadedFile':open(file, 'rb')}, data=packet).text
+      return _requests.post(self._endpoint+'submission', files={'uploadedFile':open(file, 'rb')}).text
     except:
       print('Error submitting file')
 
 # USAGE
 #
-# import pyctfsglib as ctfsg
-# grader = ctfsg.DSGraderClient("GRADER_URL", "USER_TOKEN")
+# import pyjcgraderlib as ctfgrader
+# grader = ctfgrader.DSGraderClient("GRADER_URL")
 # grader.submitFile("PATH_TO_FILE")
